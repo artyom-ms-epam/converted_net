c9789872956008571a71a37f3fe8962fa15ccfc4
artyom-ms-epam <artem_moskvin@epam.com>
2025-02-03 15:06:39 +0400

    Create SpecFlowProjectConverted/StepDefinitions/CalculatorStepDefinitions_Updated.cs

---
+++ SpecFlowProjectConverted/StepDefinitions/CalculatorStepDefinitions_Updated.cs
@@ -0,0 +1,48 @@
+namespace SpecFlowProjectConverted.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            // TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            // Remove the throw statement when the method is implemented
+            // throw new PendingStepException();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            // TODO: implement arrange (precondition) logic
+
+            // Remove the throw statement when the method is implemented
+            // throw new PendingStepException();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            // TODO: implement act (action) logic
+
+            // Remove the throw statement when the method is implemented
+            // throw new PendingStepException();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            // TODO: implement assert (verification) logic
+
+            // Remove the throw statement when the method is implemented
+            // throw new PendingStepException();
+        }
+    }
+}
